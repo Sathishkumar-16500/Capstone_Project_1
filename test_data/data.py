d61970b8213a83c9093fbb74b2c74dbc679425e3
Sathishkumar-16500 <87905560+Sathishkumar-16500@users.noreply.github.com>
2023-03-07 14:25:03 +0530

    test_data updated

--- test_data/data.py
+++ test_data/data.py
@@ -1,47 +1,21 @@
-# locators.py - File where all The HTML Locators areKept
-class Orange_hrm_Locators:
-    username_inputBox = 'username'
-    password_InputBox = 'password'
-    LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+# data.py - Keep all your Testing Data in this file
+class Orange_hrm_Data:
+    url ='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    username = 'Admin'
+    password = 'admin123'
 
-    invalid_alert = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p'
-    # error_message='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p'
+    invalid_username='Admin'
+    invalid_password='Invalid password'
 
-    pim_tab = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
-    add_button='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button'
-    firstname_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/input'
-    middlename_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/input'
-    lastname_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[2]/input'
-    employee_id_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input'
-    save_button='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]'
-    added_user_toast='//*[@id="oxd-toaster_1"]'
-
-    edit_button='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[9]/div/button[2]'
-    edit_firstname_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/input'
-    edit_middlename_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[1]/div/div/div[2]/div[2]/div[2]/input'
-    edit_lastname_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[1]/div/div/div[2]/div[3]/div[2]/input'
-    edit_employee_id_textbox='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[1]/div/div[2]/input'
-    edit_employee_details_save='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
-    edit_employee_toast='//*[@id="oxd-toaster_1"]'
-
-    delete_user_button='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[9]/div/button[1]'
-    confirm_delete_button='//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]'
-    deleted_user_toast='//*[@id="oxd-toaster_1"]'
+    test_first_name='Sathish'
+    test_middle_name='R'
+    test_last_name='Kumar'
+    test_employee_id=1260
 
 
 
-    admin_tab='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a'
-    my_info_tab='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a'
-    username_firstname='firstName'
-    username_lastname='lastName'
-    nickname='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[2]/div/div/div[2]/input'
-    employee_id='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[1]/div/div[2]/input'
-    other_id='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input'
-    license_number='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input'
-    ssn_number='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[1]/div/div[2]/input'
-    sin_number='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[3]/div[2]/div/div[2]/input'
-    nationality_dropdown='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div/div[1]'
-    marital_status='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div'
-    # save_button='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
+
+    # test_username='sathishsparkzz'
+    # test_password='Sparkzz@123'
 
 
